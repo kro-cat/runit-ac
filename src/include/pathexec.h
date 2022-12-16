92c5fd7c3938187ae82d6dd58d594a32dbafa2e4
Kara <33235324+kro-cat@users.noreply.github.com>
2022-12-16 14:06:39 -0600

    fixed all functions using execve in pathexec family
    argv and envp arguments to execve are not guaranteed const, so I've
    removed the const qualifier from those arguments in the pathexec family
    of functions. Additionally, unistd.h has been added to pathexec_run.c

--- src/include/pathexec.h
+++ src/include/pathexec.h
@@ -5,7 +5,7 @@
 
 extern void pathexec_run(const char *,char * const *,char * const *);
 extern int pathexec_env(const char *,const char *);
-extern void pathexec_env_run(const char *, const char * const *);
-extern void pathexec(const char * const *);
+extern void pathexec_env_run(const char *,char * const *);
+extern void pathexec(char * const *);
 
 #endif
