7a12af0a74a3cf1e872f8f8de02fe4fff0dadb16
Kara <33235324+kro-cat@users.noreply.github.com>
2023-08-21 14:31:30 -0500

    add missing header

--- src/include/reboot_system.h
+++ src/include/reboot_system.h
@@ -1,11 +1,12 @@
 #include <config.h>
 
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/reboot.h>
 
 int reboot_system(int what) {
 #ifdef UNGRACEFUL_DEATH
-	exit(what);
+	_exit(what);
 #else
 # ifdef NONSTDREBOOT
 	return(reboot(what, (char *)0));
